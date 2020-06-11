a5b3b0274827dd892e66f509abb5ea0be5d62001
gianmillare <gianmillaregsm@gmail.com>
2020-06-10 21:40:17 -0700

    easy += 1, added gitignore

---
+++ python/easy/remove_duplicates_from_the_array.py
@@ -0,0 +1,11 @@
+def removeDuplicates(nums):
+  index = 0
+
+  for i in range(0, len(nums)):
+    if nums[index] in nums[index+1:]:
+      nums.remove(nums[index])
+    else:
+      index += 1
+
+nums = [1,1,2]
+removeDuplicates(nums)
