ce31db8b1bee0f2dca3b6aa283980529b04ae581
Gian Millare <gianmillaregsm@gmail.com>
2020-07-10 09:25:28 -0700

    js easy += 1

---
+++ javascript/easy/merge_two_sorted_lists.js
@@ -0,0 +1,14 @@
+// 21. Merge Two Sorted Lists
+// https://leetcode.com/problems/merge-two-sorted-lists/
+
+var mergeTwoLists = function(l1, l2) {
+  if (!l1 || !l2) return (l1 ? l1 : l2);
+    
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2
+  }
+};
