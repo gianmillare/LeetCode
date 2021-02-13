42dfcdc1193a7093238f03c3ed119165b754828c
gianmillare <gianmillaregsm@gmail.com>
2021-02-13 14:07:18 -0800

    completed Richest Customer Wealth py easy

---
+++ python/easy/richest_customer_wealth.py
@@ -0,0 +1,6 @@
+# 1672. Richest Customer Wealth
+# https://leetcode.com/problems/richest-customer-wealth/
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        return max([sum(i) for i in accounts])
